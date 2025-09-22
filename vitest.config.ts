cb1278c8e952e54540c3a116ee387ff5ec5618ca
Rygent <46462403+rygent@users.noreply.github.com>
2025-09-23 05:54:48 +0700

    chore: remove vitest workspace

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,12 @@ export default createVitestConfig({
 	test: {
 		coverage: {
 			exclude: ['**/scripts/**', 'commitlint.config.js', 'eslint.config.js', 'vitest.workspace.ts']
-		}
+		},
+		projects: [
+			'./packages/anilist/vitest.config.ts',
+			'./packages/igdb/vitest.config.ts',
+			'./packages/spotify/vitest.config.ts'
+		]
 	},
 	esbuild: {
 		target: 'esnext'
--- vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-	'./packages/anilist/vitest.config.ts',
-	'./packages/igdb/vitest.config.ts',
-	'./packages/spotify/vitest.config.ts'
-]);
